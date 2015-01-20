297f408ab2d470b78a5871ec41680f76683f8806
Mike <mikesigsworth@hotmail.com>
2015-01-20 00:46:29 -0600

    Added remote state to prompt
    get_remote_state returns a string similar to the result of `git status -sb` and appends it to the existing cmder prompt. This allows you to see at a glance, not only which branch you are on and its dirty state, but also the number of commits ahead and behind the remote repository you are. Here's a screenshot of it in action - http://i.imgur.com/mk0ahik.png

--- config/git.lua
+++ config/git.lua
@@ -1,8 +1,25 @@
 ---
+-- This file is intended to be used with cmder
+-- http://bliker.github.io/cmder/
+--
+-- Replace your existing C:\bin\cmder\config\git.lua with this file
+-- This will append the branch status to your prompt, e.g. [ahead 1, behind 2]
+-- It basically takes the output of git status -sb and appends it (with coloring) to the cmder prompt (before the lambda)
+---
+
+---
+-- Colors for git status
+---
+default = "\x1b[1;32;40m";
+green = "\x1b[0;32;40m";
+white = "\x1b[1;37;40m";
+red = "\x1b[1;31;40m";
+
+---
  -- Find out current branch
  -- @return {false|git branch name}
 ---
-function get_git_branch()
+function get_branch_name()
     for line in io.popen("git branch 2>nul"):lines() do
         local m = line:match("%* (.+)$")
         if m then
@@ -14,31 +31,57 @@ function get_git_branch()
 end
 
 ---
- -- Get the status of working dir
- -- @return {bool}
+ -- Get the remote state for the current branch
+ -- @return {string}
 ---
-function get_git_status()
-    return os.execute("git diff --quiet --ignore-submodules HEAD")
+function get_remote_state()
+    local branch_status = io.popen("git status -sb 2>nul"):read("*l")
+    local ahead_num = branch_status:match(".*%[.*ahead (%d+).*%]")
+    local behind_num = branch_status:match(".*%[.*behind (%d+).*%]")
+    local remote_state = { }
+    
+    if ahead_num or behind_num then
+        table.insert(remote_state, white.." [")
+        if ahead_num then
+            table.insert(remote_state, "ahead "..green..ahead_num..white)
+            if behind_num then
+                table.insert(remote_state, ", ")
+            end
+        end
+        if behind_num then
+            table.insert(remote_state, "behind "..red..behind_num..white)
+        end
+        table.insert(remote_state, "]")
+    end 
+
+    return table.concat(remote_state)
 end
 
+
+---
+ -- Get the color to use for the batch prompt
+ -- @return {string}
+---
+function get_branch_color()
+    local is_dirty = os.execute("git diff --quiet --ignore-submodules HEAD")    
+    if is_dirty then
+        return white
+    else
+        return red
+    end
+end
+
+---
+-- Filter function to register with clink
+---
 function git_prompt_filter()
-
-    -- Colors for git status
-    local colors = {
-        clean = "\x1b[1;37;40m",
-        dirty = "\x1b[31;1m",
-    }
-
-    local branch = get_git_branch()
+    local branch = get_branch_name()
     if branch then
-        -- Has branch => therefore it is a git folder, now figure out status
-        if get_git_status() then
-            color = colors.clean
-        else
-            color = colors.dirty
-        end
-
-        clink.prompt.value = string.gsub(clink.prompt.value, "{git}", color.."("..branch..")")
+        local branch_color = get_branch_color()
+        local remote_state = get_remote_state()
+                
+        clink.prompt.value = string.gsub(clink.prompt.value, "{git}", branch_color.."("..branch..")"..remote_state..default)
+        
         return true
     end
 
